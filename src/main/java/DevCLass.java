5cc2da37b6fd22d9b4a5de12ebebbbf4f76a6006
topsettle <119459641+topsettle@users.noreply.github.com>
2022-11-29 23:53:41 +0100

    Update DevCLass.java

--- src/main/java/DevCLass.java
+++ src/main/java/DevCLass.java
@@ -2,6 +2,7 @@ public class DevCLass {
 
     public static void main(String[] args)
     {
-        System.out.println("DevClass");
+        System.out.println("DevClass+TopSettle");
+        //Checkign from topsettle
     }
 }
